c7982e82b521a96dcc8031adedddab1828876d3b
Bitpound <mox_egy@yahoo.com>
2021-11-11 23:37:02 +0200

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,7 +4,8 @@ import { Menu as UikitMenu } from '@pancakeswap/uikit'
 import { languageList } from 'config/localization/languages'
 import { useTranslation } from 'contexts/Localization'
 import useTheme from 'hooks/useTheme'
-import { usePriceCakeBusd } from 'state/farms/hooks'
+// import { usePriceCakeBusd } from 'state/farms/hooks'
+import { usePriceCakeBusd } from 'hooks/useBUSDPrice'
 import config from './config/config'
 import UserMenu from './UserMenu'
 import GlobalSettings from './GlobalSettings'
@@ -13,7 +14,8 @@ import { footerLinks } from './config/footerConfig'
 
 const Menu = (props) => {
   const { isDark, toggleTheme } = useTheme()
-  const cakePriceUsd = usePriceCakeBusd()
+  // const cakePriceUsd = usePriceCakeBusd()
+  const cakePriceUsd = useCakeBusdPrice()
   const { currentLanguage, setLanguage, t } = useTranslation()
   const { pathname } = useLocation()
 
