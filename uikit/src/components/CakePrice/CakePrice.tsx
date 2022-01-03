b4a7a3759d5291e5cb9ea0a8186c51e0661a1c70
Sniper <mox_egy@yahoo.com>
2022-01-03 03:50:17 +0200

    Update CakePrice.tsx

--- uikit/src/components/CakePrice/CakePrice.tsx
+++ uikit/src/components/CakePrice/CakePrice.tsx
@@ -26,8 +26,7 @@ const PriceLink = styled.a`
 const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
   return cakePriceUsd ? (
     <PriceLink
-      href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
-      target="_blank"
+      href="http://sniperswap.com:5000/swap?outputCurrency=0x031f7e8a0f17746f797cb4f7c0f18e99dee2aa96"
     >
       <PancakeRoundIcon width="24px" mr="8px" />
       <Text color={color} bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
